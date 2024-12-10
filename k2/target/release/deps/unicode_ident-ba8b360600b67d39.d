14ed9e425ab77382a48446208c1e0fcfdd7f2b0c
RedMage08 <theredmage08@gmail.com>
2024-12-09 23:02:11 -0700

    Delete k2 directory

--- k2/target/release/deps/unicode_ident-ba8b360600b67d39.d
+++
@@ -1,8 +0,0 @@
-D:\Kotor-Livesplit-shit\k2\target\release\deps\libunicode_ident-ba8b360600b67d39.rmeta: C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
-
-D:\Kotor-Livesplit-shit\k2\target\release\deps\libunicode_ident-ba8b360600b67d39.rlib: C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
-
-D:\Kotor-Livesplit-shit\k2\target\release\deps\unicode_ident-ba8b360600b67d39.d: C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs
-
-C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src/lib.rs:
-C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.14\src\tables.rs:
--- k2/target/wasm32-unknown-unknown/release/deps/num_conv-28236fb8bd571b71.d
+++
@@ -1,7 +0,0 @@
-D:\Kotor-Livesplit-shit\k2\target\wasm32-unknown-unknown\release\deps\libnum_conv-28236fb8bd571b71.rmeta: C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-conv-0.1.0\src\lib.rs
-
-D:\Kotor-Livesplit-shit\k2\target\wasm32-unknown-unknown\release\deps\libnum_conv-28236fb8bd571b71.rlib: C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-conv-0.1.0\src\lib.rs
-
-D:\Kotor-Livesplit-shit\k2\target\wasm32-unknown-unknown\release\deps\num_conv-28236fb8bd571b71.d: C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-conv-0.1.0\src\lib.rs
-
-C:\Users\jacob\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-conv-0.1.0\src\lib.rs:
